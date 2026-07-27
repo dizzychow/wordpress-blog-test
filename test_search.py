3cfef0dbb60a14525abb7c3f752342398213e4b5
dizzychow <dizzychow221@gmail.com>
2026-07-27 23:02:28 +0800

    Reformat test_search.py for consistency

--- test_search.py
+++ test_search.py
@@ -10,11 +10,9 @@ def test_search_article(driver):
     driver.get(BASE_URL)
     wait = WebDriverWait(driver, 10)
 
-    # 等待搜索框可见
     search_input = wait.until(EC.visibility_of_element_located((By.NAME, "s")))
     search_input.send_keys(SEARCH_TERM)
     search_input.submit()
 
-    # 等待搜索结果
     wait.until(EC.presence_of_element_located((By.CLASS_NAME, "search-results")))
     assert SEARCH_TERM in driver.page_source
