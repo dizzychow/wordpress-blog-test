32075f43051848b16ef2bb48619f179a13ca9d6c
dizzychow <dizzychow221@gmail.com>
2026-07-27 23:01:51 +0800

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -4,14 +4,13 @@ from selenium.webdriver.edge.service import Service
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.edge.options import Options as EdgeOptions
 
-BASE_URL = "http://120.25.77.137"          # 注意：这里是 http 不是 https
+BASE_URL = "http://120.25.77.137" 
 ADMIN_USER = "DzChow"
 ADMIN_PASS = "&0vyD*!HyaN)FJq7VL"
 
 @pytest.fixture(scope="function")
 def driver():
     edge_options = EdgeOptions()
-    # 关键：忽略证书错误、禁用安全策略、允许不安全内容
     edge_options.add_argument("--ignore-certificate-errors")
     edge_options.add_argument("--allow-running-insecure-content")
     edge_options.add_argument("--disable-web-security")
